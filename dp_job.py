3fcf384ddda5c0331c6df82dc3cf246ff32c610b
John Bodenschatz <John Bodenschatz john.bodenschatz@cern.ch>
2020-06-19 20:42:27 +0200

    minor edits before running 6-19:3

--- dp_job.py
+++ dp_job.py
@@ -1,4 +1,4 @@
-j = Job(name='6-19:1 Dp_KKpi') #"mm-dd:run decay"
+j = Job(name='6-19:3 Dp_KKpi') #"mm-dd:run decay"
 myApp = GaudiExec()
 myApp.directory = "./DaVinciDev_v42r5" #davinci version listed for charmcharged stream
 j.application = myApp
@@ -6,9 +6,9 @@ j.application.options = ['Dp_KKpi_options.py'] #ntuple options file
 j.application.platform = 'x86_64-centos7-gcc62-opt' #gcc62 to match davinci version, usually gcc8
 bkPath = '/LHCb/Collision17/Beam6500GeV-VeloClosed-MagDown/Real Data/Turbo04/94000000/CHARMCHARGED.MDST' #path to real data
 data = BKQuery(bkPath, dqflag=['OK']).getDataset()
-j.inputdata = data[0:4] #get first 4 data files (out of 4311)
+j.inputdata = data[0:2] #get first 4 data files (out of 4311)
 j.backend = Dirac()
-j.splitter = SplitByFiles(filesPerJob=2) #should give 2 subjobs (4 files / 2 files per job)
+j.splitter = SplitByFiles(filesPerJob=1) #should give 2 subjobs (2 files / 1 files per job)
 j.outputfiles = [DiracFile('DVntuple.root')] #store on eos
 j.submit()
 
