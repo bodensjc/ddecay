75b9c4c4f0f516c989836af83e5b9b3e6ceb7f5f
John Bodenschatz <John Bodenschatz john.bodenschatz@cern.ch>
2020-06-19 17:59:12 +0200

    fixed some pathing for DaVinci. dddb tags still under inspection

--- dp_job.py
+++ dp_job.py
@@ -1,14 +1,14 @@
-j = Job(name='6-18:1 Dp_KKpi')
+j = Job(name='6-19:1 Dp_KKpi')
 myApp = GaudiExec()
-myApp.directory = "./DaVinci/v42r5"
+myApp.directory = "./DaVinciDev_v42r5"
 j.application = myApp
 j.application.options = ['Dp_KKpi_options.py'] #ntuple options file
 j.application.platform = 'x86_64-centos7-gcc62-opt' #maybe gcc8
 bkPath = '/LHCb/Collision17/94000000/Beam6500GeV-VeloClosed-MagDown/Real Data/Turbo04/CHARMCHARGED.MDST'
 data = BKQuery(bkPath, dqflag=['OK']).getDataset()
-j.inputdata = data[0:10] #get first 10 data files (out of 4311)
+j.inputdata = data[0:4] #get first 4 data files (out of 4311)
 j.backend = Dirac()
-j.splitter = SplitByFiles(filesPerJob=5) #should give 2 subjobs (10 files / 5 files per job)
+j.splitter = SplitByFiles(filesPerJob=2) #should give 2 subjobs (4 files / 2 files per job)
 j.outputfiles = [DiracFile('DVntuple.root')] #store on eos
 j.submit()
 
