a50cb0a610155e28b9288695fd559684e8a4531c
John Bodenschatz <John Bodenschatz john.bodenschatz@cern.ch>
2020-06-20 20:26:24 +0200

    Finally fixed this piece of junk, newlines had a hidden character

--- Dp_KKpi_options.py
+++ Dp_KKpi_options.py
@@ -2,26 +2,26 @@
 from GaudiConf import IOHelper
 from Configurables import DaVinci, DecayTreeTuple
 from DecayTreeTuple.Configuration import *
-​
+
 stream = 'Charmcharged'
 line = 'Hlt2CharmHadDpToKmKpPipTurbo'
-​
+
 dtt = DecayTreeTuple('D2KKpi_Tuple')
 dtt.Inputs = ['{0}/Particles'.format(line)] #/Event/{0}/Phys/{1}/Particles
 dtt.Decay = '[D+ -> ^K- ^K+ ^pi+]CC'
 track_tool = dtt.addTupleTool('TupleToolTrackInfo')
 track_tool.Verbose = True
 dtt.addTupleTool('TupleToolPrimaries')
-​
+
 dtt.addBranches({
         'Dplus' : '([^D+ -> K- K+ pi+]CC)',
         'Kminus' : '([D+ -> ^K- K+ pi+]CC)',
         'Kplus' : '([D+ -> K- ^K+ pi+]CC)',
         'Piplus' : '([D+ -> K- K+ ^pi+]CC)'
 })
-​
+
 DaVinci().UserAlgorithms += [dtt]
-​
+
 DaVinci().Turbo = True
 DaVinci().RootInTES = '/Event/Charmcharged/Turbo'
 DaVinci().InputType = 'MDST'
@@ -32,6 +32,6 @@ DaVinci().PrintFreq = 5000
 DaVinci().DataType = '2017'
 DaVinci().Lumi = False
 DaVinci().EvtMax = -1
-​
-#IOHelper().inputFiles([
-#    'root://hake1.grid.surfsara.nl:1094/pnfs/grid.sara.nl/data/lhcb/LHCb/Collision17/CHARMCHARGED.MDST/00067727/0001/00067727_00012788_1.charmcharged.mdst'], clear=True)
+
+IOHelper().inputFiles([
+    'root://hake1.grid.surfsara.nl:1094/pnfs/grid.sara.nl/data/lhcb/LHCb/Collision17/CHARMCHARGED.MDST/00067727/0001/00067727_00012788_1.charmcharged.mdst'], clear=True)
