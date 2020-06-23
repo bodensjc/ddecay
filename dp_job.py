453650db5a6d1545965359741e9b88b987d09ee6
John Bodenschatz <John Bodenschatz john.bodenschatz@cern.ch>
2020-06-23 18:10:06 +0200

    Final revision for Dp->KKpi.

--- dp_job.py
+++ dp_job.py
@@ -1,4 +1,4 @@
-j = Job(name='6-20:5 Dp_KKpi') #"mm-dd:run decay"
+j = Job(name='6-23:1 Dp_KKpi') #"mm-dd:run decay"
 myApp = GaudiExec()
 myApp.directory = "./DaVinciDev_v45r1 " #davinci version listed for charmcharged stream is v42r5 but this oen works
 j.application = myApp
@@ -6,11 +6,11 @@ j.application.options = ['Dp_KKpi_options.py'] #ntuple options file
 j.application.platform = 'x86_64-centos7-gcc8-opt' #gcc8 to match davinci version, gcc62 for v42r5
 bkPath = '/LHCb/Collision17/Beam6500GeV-VeloClosed-MagDown/Real Data/Turbo04/94000000/CHARMCHARGED.MDST' #path to real data
 data = BKQuery(bkPath, dqflag=['OK']).getDataset()
-j.inputdata = data[0:2] #get first 2 data files (out of 4311)
+j.inputdata = data[0:10] #get first 10 data files (out of 4311)
 j.backend = Dirac()
 j.outputfiles = [
-	LocalFile('Dp2KKpiMagDown.root')
+	DiracFile('Dp2KKpiMagDown.root')
 ]
-j.splitter = SplitByFiles(filesPerJob=1) #should give 2 subjobs (2 files / 1 files per job)
+j.splitter = SplitByFiles(filesPerJob=2) #should give 5 subjobs (10 files / 2 files per job)
 j.submit()
 
