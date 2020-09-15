6eaed82484fc072972a560c90034cc2b152ecf07
John Bodenschatz <47458099+bodensjc@users.noreply.github.com>
2020-09-15 13:37:45 -0400

    trying charmspec instead of charmcharged for dsp

--- dsp_job.py
+++ dsp_job.py
@@ -1,10 +1,10 @@
-j = Job(name='dsp_9-12_test3') #"mm-dd:run decay"
+j = Job(name='dsp_9-15_test1') #"mm-dd:run decay"
 myApp = GaudiExec()
 myApp.directory = "./DaVinciDev_v45r1 " #davinci version listed for charmcharged stream is v42r5 but this oen works
 j.application = myApp
 j.application.options = ['dsp_options.py'] #ntuple options file
 j.application.platform = 'x86_64-centos7-gcc8-opt' #gcc8 to match davinci version, gcc62 for v42r5
-bkPath = '/LHCb/Collision17/Beam6500GeV-VeloClosed-MagDown/Real Data/Turbo04/94000000/CHARMCHARGED.MDST' #path to real data
+bkPath = '/LHCb/Collision17/Beam6500GeV-VeloClosed-MagDown/Real Data/Turbo04/94000000/CHARMSPEC.MDST' #path to real data
 data = BKQuery(bkPath, dqflag=['OK']).getDataset()
 j.inputdata = data[0:5] #get first 5 data files to make sure this job/options works (change to 4311 if it works)
 j.backend = Dirac()
