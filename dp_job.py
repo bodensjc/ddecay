47ad82d85978e406a8576e3a5b41af9659297e02
John Bodenschatz <47458099+bodensjc@users.noreply.github.com>
2020-09-16 11:13:09 -0400

    pack it and ship it fellas, final big jobs for dp and dsp

--- dp_job.py
+++ dp_job.py
@@ -1,4 +1,4 @@
-j = Job(name='dp_9-15_test1') #"mm-dd:run decay"
+j = Job(name='dp_9-16') #"mm-dd:run decay"
 myApp = GaudiExec()
 myApp.directory = "./DaVinciDev_v45r1 " #davinci version listed for charmcharged stream is v42r5 but this oen works
 j.application = myApp
@@ -6,7 +6,7 @@ j.application.options = ['dp_options.py'] #ntuple options file
 j.application.platform = 'x86_64-centos7-gcc8-opt' #gcc8 to match davinci version, gcc62 for v42r5
 bkPath = '/LHCb/Collision17/Beam6500GeV-VeloClosed-MagDown/Real Data/Turbo04/94000000/CHARMCHARGED.MDST' #path to real data
 data = BKQuery(bkPath, dqflag=['OK']).getDataset()
-j.inputdata = data[0:5] #get first 5 data files to make sure this job/options works (change to 4311 if it works)
+j.inputdata = data[0:4310] #get all data files
 j.backend = Dirac()
 
 #can use the below line to not run on russian servers if its going to be an issue again...
