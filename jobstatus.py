85b29fbde67fb70c39cd289161b73a9315f20c7a
John Bodenschatz <47458099+bodensjc@users.noreply.github.com>
2020-09-23 17:53:33 -0400

    file to get summary of subjob status

---
+++ jobstatus.py
@@ -0,0 +1,12 @@
+#print out the count of subjob statuses, "ganga jobstatus.py 73" 73 is job#
+
+import sys
+from collections import Counter
+list=[]
+jobNumber = int(sys.argv[1])
+for sj in jobs(jobNumber).subjobs:
+    list = list+[sj.status]
+length = len(list)
+a = dict(Counter(list))
+print("Total of {0} subjobs\n".format(length))
+print(a)
