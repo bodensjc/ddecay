6e9065f5abf029b7ce712b0e9897c2652b1a7254
John Bodenschatz <47458099+bodensjc@users.noreply.github.com>
2020-10-29 14:37:28 -0400

    updating jobs and options to get MagUp data

--- dp_job.py
+++ dp_job.py
@@ -1,10 +1,10 @@
-j = Job(name='dp_9-29') #"mm-dd:run decay"
+j = Job(name='dp_10-29') #"mm-dd:run decay"
 myApp = GaudiExec()
 myApp.directory = "./DaVinciDev_v45r1 " #davinci version listed for charmcharged stream is v42r5 but this oen works
 j.application = myApp
 j.application.options = ['dp_options.py'] #ntuple options file
 j.application.platform = 'x86_64-centos7-gcc8-opt' #gcc8 to match davinci version, gcc62 for v42r5
-bkPath = '/LHCb/Collision17/Beam6500GeV-VeloClosed-MagDown/Real Data/Turbo04/94000000/CHARMCHARGED.MDST' #path to real data
+bkPath = '/LHCb/Collision17/Beam6500GeV-VeloClosed-MagUp/Real Data/Turbo04/94000000/CHARMCHARGED.MDST' #path to real data
 data = BKQuery(bkPath, dqflag=['OK']).getDataset()
 j.inputdata = data[0:4310] #get all data files
 j.backend = Dirac()
@@ -13,7 +13,7 @@ j.backend = Dirac()
 #j.backend.settings['BannedSites'] = ['LCG.Some.Site','LCG.SomeOther.Site'] 
 
 j.outputfiles = [
-	DiracFile('Dp2KKpiMagDown.root')
+	DiracFile('Dp2KKpiMagUp.root')
 ]
 j.splitter = SplitByFiles(filesPerJob=5) 
 j.submit()
--- dsp_job.py
+++ dsp_job.py
@@ -1,10 +1,10 @@
-j = Job(name='dsp_9-29') #"mm-dd:run"
+j = Job(name='dsp_10-29') #"mm-dd:run"
 myApp = GaudiExec()
 myApp.directory = "./DaVinciDev_v45r1 " #davinci version listed for charmcharged stream is v42r5 but this oen works
 j.application = myApp
 j.application.options = ['dsp_options.py'] #ntuple options file
 j.application.platform = 'x86_64-centos7-gcc8-opt' #gcc8 to match davinci version, gcc62 for v42r5
-bkPath = '/LHCb/Collision17/Beam6500GeV-VeloClosed-MagDown/Real Data/Turbo04/94000000/CHARMSPEC.MDST' #path to real data
+bkPath = '/LHCb/Collision17/Beam6500GeV-VeloClosed-MagUp/Real Data/Turbo04/94000000/CHARMSPEC.MDST' #path to real data
 data = BKQuery(bkPath, dqflag=['OK']).getDataset()
 j.inputdata = data[0:12693] #get first 5 data files
 j.backend = Dirac()
@@ -13,7 +13,7 @@ j.backend = Dirac()
 #j.backend.settings['BannedSites'] = ['LCG.Some.Site','LCG.SomeOther.Site'] 
 
 j.outputfiles = [
-	DiracFile('Dsp2KKpiMagDown.root')
+	DiracFile('Dsp2KKpiMagUp.root')
 ]
 j.splitter = SplitByFiles(filesPerJob=5) 
 j.submit()
