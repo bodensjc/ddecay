38565a92fc52165135a64e5cedb88ad2efb2ddd3
John Bodenschatz <47458099+bodensjc@users.noreply.github.com>
2020-12-16 12:45:51 -0500

    fixing job for current davinci

--- dp_job.py
+++ dp_job.py
@@ -1,6 +1,10 @@
-j = Job(name='dp_12-16:0') #"mm-dd:run decay"
-myApp = prepareGaudiExec('DaVinci', 'v45r1', myPath='.')
-#myApp.directory = "./DaVinciDev_v45r1" #davinci version listed for charmcharged stream is v42r5 but this oen works
+j = Job(name='dp_12-16:1') #"mm-dd:run decay"
+#only need below line if its first time running with DaVinci version
+#myApp = prepareGaudiExec('DaVinci', 'v45r1', myPath='.')
+
+myApp = GaudiExec()
+myApp.directory = "./DaVinciDev_v45r1"
+
 j.application = myApp
 j.application.options = ['dp_options.py'] #ntuple options file
 j.application.platform = 'x86_64-centos7-gcc8-opt' #gcc8 to match davinci version, gcc62 for v42r5
@@ -12,9 +16,8 @@ j.backend = Dirac()
 #can use the below line to not run on russian servers if its going to be an issue again...
 #j.backend.settings['BannedSites'] = ['LCG.Some.Site','LCG.SomeOther.Site'] 
 
-j.outputfiles = [
-	DiracFile('Dp2KKpiMagUp.root')
-]
+
 j.splitter = SplitByFiles(filesPerJob=5) 
+j.outputfiles = [DiracFile('Dp2KKpiMagUp.root')]
 j.submit()
 
