6622017de58134c98dcfce3a5b6618d51a409e7b
John Bodenschatz <47458099+bodensjc@users.noreply.github.com>
2020-12-16 21:27:52 -0500

    updating jobs and ntuple options for momentu scaling data on magup data

--- dp_job.py
+++ dp_job.py
@@ -1,4 +1,4 @@
-j = Job(name='dp_12-16:1') #"mm-dd:run decay"
+j = Job(name='dp_up_12-16:all') #"mm-dd:run"
 #only need below line if its first time running with DaVinci version
 #myApp = prepareGaudiExec('DaVinci', 'v45r1', myPath='.')
 
@@ -10,12 +10,11 @@ j.application.options = ['dp_options.py'] #ntuple options file
 j.application.platform = 'x86_64-centos7-gcc8-opt' #gcc8 to match davinci version, gcc62 for v42r5
 bkPath = '/LHCb/Collision17/Beam6500GeV-VeloClosed-MagUp/Real Data/Turbo04/94000000/CHARMCHARGED.MDST' #path to real data
 data = BKQuery(bkPath, dqflag=['OK']).getDataset()
-j.inputdata = data[0:3] #get all data files
+j.inputdata = data[0:4126] #get all data files (magup)
 j.backend = Dirac()
 
 #can use the below line to not run on russian servers if its going to be an issue again...
 #j.backend.settings['BannedSites'] = ['LCG.Some.Site','LCG.SomeOther.Site'] 
-
 
 j.splitter = SplitByFiles(filesPerJob=5) 
 j.outputfiles = [DiracFile('Dp2KKpiMagUp.root')]
