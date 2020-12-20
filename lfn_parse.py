2cc087d442d3898d4798a5779770aa7e4a138456
John Bodenschatz <bodensjc@mail.uc.edu>
2020-12-20 13:57:02 -0500

    dp parsed lfns

--- lfn_parse.py
+++ lfn_parse.py
@@ -3,20 +3,52 @@ os.chdir('C:\\Users\\johnb\\Documents\\LHCb\\LHCb')
 
 
 
-lfn = open('maguplfns.txt','r')
-dplfn = open('dplfns.txt', 'w')
-dsplfn = open('dsplfns.txt','w')
+lfn = open('dp_lfns_raw.txt','r')
+magup = open('dp_lfns_magup.txt', 'w')
+magdown = open('dp_lfns_magdown.txt', 'w')
 
 lines = lfn.readlines()
 
+
+upi=0
+downi=0
+upj=1
+downj=1
+magup.write('dpmagup{0}.root\n'.format(upj))
+magdown.write('dpmagdown{0}.root\n'.format(downj))
+for line in lines:
+    up = line.find('MagUp')
+    down = line.find('MagDown')
+    if up != -1:
+        line = line.replace('\n',' ')
+        magup.write(line[up+13:])
+        upi += 1
+        if upi == 60:
+            upj+=1
+            magup.write('\n\ndpmagup{0}.root\n'.format(upj))
+            upi=0
+    if down != -1:
+        line = line.replace('\n',' ')
+        magdown.write(line[down+15:])
+        downi += 1
+        if downi == 60:
+            downj+=1
+            magdown.write('\n\ndpmagdown{0}.root\n'.format(downj))
+            downi=0
+    else:
+        pass
+
+
+
+
+'''
+#second oldest parser
 dspi=0
 dpi=0
 dspj=1
 dpj=1
-
 dsplfn.write('dsp{0}.root\n'.format(dspj))
 dplfn.write('dp{0}.root\n'.format(dpj))
-
 for line in lines:
     dsp = line.find('Dsp')
     dp = line.find('Dp')
@@ -40,11 +72,10 @@ for line in lines:
 
     else:
         pass
-
-
 '''
 
-
+'''
+#oldest parser
 dplfn = open('dplfns.txt', 'r')
 newdp = open('dplfns2.txt', 'w')
 
