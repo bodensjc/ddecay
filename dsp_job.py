9c1d79518e8e75637a7eb8113bab2a7f2e1d87cb
John Bodenschatz <bodensjc@mail.uc.edu>
2021-01-01 14:43:03 -0500

    dsp mag down run

--- dsp_job.py
+++ dsp_job.py
@@ -1,4 +1,4 @@
-j = Job(name='dsp_up_1-1:0') #"mm-dd:run"
+j = Job(name='dsp_down_1-1:0') #"mm-dd:run"
 #only need below line if its first time running with DaVinci version
 #myApp = prepareGaudiExec('DaVinci', 'v45r1', myPath='.')
 
@@ -8,16 +8,17 @@ myApp.directory = "./DaVinciDev_v45r1"
 j.application = myApp
 j.application.options = ['dsp_options.py'] #ntuple options file
 j.application.platform = 'x86_64-centos7-gcc8-opt' #gcc8 to match davinci version, gcc62 for v42r5
-bkPath = '/LHCb/Collision17/Beam6500GeV-VeloClosed-MagUp/Real Data/Turbo04/94000000/CHARMSPEC.MDST' #path to real data
-#magdown path'/LHCb/Collision17/Beam6500GeV-VeloClosed-MagDown/Real Data/Turbo04/94000000/CHARMCHARGED.MDST'
+bkPath = '/LHCb/Collision17/Beam6500GeV-VeloClosed-MagDown/Real Data/Turbo04/94000000/CHARMCHARGED.MDST' #path to real data
+#magdown path: '/LHCb/Collision17/Beam6500GeV-VeloClosed-MagDown/Real Data/Turbo04/94000000/CHARMCHARGED.MDST'
+#magup path: '/LHCb/Collision17/Beam6500GeV-VeloClosed-MagUp/Real Data/Turbo04/94000000/CHARMSPEC.MDST'
 data = BKQuery(bkPath, dqflag=['OK']).getDataset()
-j.inputdata = data[0:13551] #get all data files (magup - 13551) (magdown - 12693)
+j.inputdata = data[0:12693] #get all data files (magup - 13551) (magdown - 12693)
 j.backend = Dirac()
 
 #can use the below line to not run on russian servers if its going to be an issue again...
 #j.backend.settings['BannedSites'] = ['LCG.Some.Site','LCG.SomeOther.Site'] 
 
 j.splitter = SplitByFiles(filesPerJob=5) 
-j.outputfiles = [DiracFile('Dsp2KKpiMagUp.root')]
+j.outputfiles = [DiracFile('Dsp2KKpiMagDown.root')] #change to magdown/magup
 j.submit()
 
