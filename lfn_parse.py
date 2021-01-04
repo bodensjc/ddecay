daf59710a8834abb427f5b56ae9e72f0092c13e9
John Bodenschatz <bodensjc@mail.uc.edu>
2021-01-03 22:11:55 -0500

    new lfns for dsp. final set if all goes well!

--- lfn_parse.py
+++ lfn_parse.py
@@ -3,13 +3,14 @@ os.chdir('C:\\Users\\johnb\\Documents\\LHCb\\LHCb')
 
 
 
-lfn = open('dp_lfns_raw.txt','r')
-magup = open('dp_lfns_magup.txt', 'w')
-magdown = open('dp_lfns_magdown.txt', 'w')
+lfn = open('dplfns_dec20.txt','r')
+magup = open('dplfns_magup.txt', 'w')
+magdown = open('dplfns_magdown.txt', 'w')
 
 lines = lfn.readlines()
 
 
+#dplus parser
 upi=0
 downi=0
 upj=1
@@ -41,55 +42,39 @@ for line in lines:
 
 
 
-'''
-#second oldest parser
-dspi=0
-dpi=0
-dspj=1
-dpj=1
-dsplfn.write('dsp{0}.root\n'.format(dspj))
-dplfn.write('dp{0}.root\n'.format(dpj))
+
+lfn = open('dsplfns_dec20.txt','r')
+magup = open('dsplfns_magup.txt', 'w')
+magdown = open('dsplfns_magdown.txt', 'w')
+
+lines = lfn.readlines()
+
+
+#dsplus parser
+upi=0
+downi=0
+upj=1
+downj=1
+magup.write('dspmagup{0}.root\n'.format(upj))
+magdown.write('dspmagdown{0}.root\n'.format(downj))
 for line in lines:
-    dsp = line.find('Dsp')
-    dp = line.find('Dp')
-
-    if dsp != -1:
-        line=line.replace('\n',' ')
-        dsplfn.write(line[dsp+21:])
-        dspi += 1
-        if dspi==60:
-            dspj+=1
-            dsplfn.write('\n\ndsp{0}.root\n'.format(dspj))
-            dspi=0     
-    if dp != -1:
-        line=line.replace('\n',' ')
-        dplfn.write(line[dp+20:])
-        dpi += 1
-        if dpi==60:
-            dpj+=1
-            dplfn.write('\n\ndp{0}.root\n'.format(dpj))
-            dpi=0
-
+    up = line.find('MagUp')
+    down = line.find('MagDown')
+    if up != -1:
+        line = line.replace('\n',' ')
+        magup.write(line[up+13:])
+        upi += 1
+        if upi == 60:
+            upj+=1
+            magup.write('\n\ndspmagup{0}.root\n'.format(upj))
+            upi=0
+    if down != -1:
+        line = line.replace('\n',' ')
+        magdown.write(line[down+16:])
+        downi += 1
+        if downi == 60:
+            downj+=1
+            magdown.write('\n\ndspmagdown{0}.root\n'.format(downj))
+            downi=0
     else:
         pass
-'''
-
-'''
-#oldest parser
-dplfn = open('dplfns.txt', 'r')
-newdp = open('dplfns2.txt', 'w')
-
-lines = dplfn.readlines()
-soct = len(lines)//60
-
-i=0
-for line in lines:
-    line = line.replace('\n',' ')
-    newdp.write(line)
-    i+=1
-    if i==60:
-        newdp.write('\n\n\n')
-        i=0
-
-
-'''
