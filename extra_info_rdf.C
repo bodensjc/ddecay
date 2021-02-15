f624e8cd7d85d2f439fdfe07c73bff16f338794d
John Bodenschatz(Mike student) <bodensjc@mail.uc.edu>
2021-02-14 22:53:30 -0500

    runNumber review

--- extra_info_rdf.C
+++ extra_info_rdf.C
@@ -50,7 +50,6 @@ RDataFrame dspdf("Dsp2KKpi/DecayTree", "/share/lazy/D2KKpi/dsp_data_dec20/dspmag
 
 
 //cut the data
-
 auto dp_cut = dpdf.Filter(cut_ipchi2, {"Dplus_IPCHI2_OWNPV"})
 				   .Filter(cut_fdchi2, {"Dplus_FDCHI2_OWNPV"})
 				   .Filter(cut_endvertexchi2, {"Dplus_ENDVERTEX_CHI2"})
@@ -163,6 +162,7 @@ auto dsp_cut = dspdf.Filter(cut_ipchi2, {"Dsplus_IPCHI2_OWNPV"})
 	}
 */
 //these values are the overlap region as produced above
+/*
 	int MeV[81] = {1878, 1879, 1880, 1881, 1882, 1883, 1884, 1885, 1886, 1887, 1888, 1889, 1890, 1891, 1892, 1893, 1894, 1895, 1896, 1897, 1898, 1899, 1900, 1901, 1902, 1903, 1904, 1905, 1906, 1907, 1908, 1909, 1910, 1911, 1912, 1913, 1914, 1915, 1916, 1917, 1918, 1919, 1920, 1921, 1922, 1923, 1924, 1925, 1926, 1927, 1928, 1929, 1930, 1931, 1932, 1933, 1934, 1935, 1936, 1937, 1938, 1939, 1940, 1941, 1942, 1943, 1944, 1945, 1946, 1947, 1948, 1949, 1950, 1951, 1952, 1953, 1954, 1955, 1956, 1957, 1958};
 	int nD[81] = {86014, 65453, 49013, 36850, 27061, 19801, 14523, 10895, 8124, 6395, 4969, 3993, 3278, 2618, 2267, 1936, 1769, 1605, 1491, 1447, 1335, 1338, 1264, 1254, 1139, 1118, 1146, 1142, 1132, 1145, 1112, 1148, 1122, 1161, 1165, 1147, 1180, 1205, 1255, 1309, 1338, 1328, 1290, 1344, 1420, 1473, 1486, 1524, 1548, 1614, 1636, 1709, 1811, 1829, 1917, 2030, 2128, 2170, 2288, 2477, 2646, 2695, 2929, 3319, 3470, 4051, 4140, 4843, 5523, 6116, 5797, 1627, 271, 91, 44, 23, 15, 5, 5, 3, 0};
 	int nDs[81] = {0, 3, 7, 8, 5, 16, 34, 41, 82, 231, 933, 2781, 2946, 2489, 2213, 1900, 1759, 1602, 1485, 1450, 1339, 1341, 1267, 1255, 1142, 1123, 1156, 1155, 1133, 1147, 1114, 1152, 1126, 1165, 1170, 1156, 1188, 1206, 1257, 1315, 1344, 1335, 1294, 1356, 1428, 1478, 1487, 1534, 1560, 1621, 1642, 1719, 1813, 1835, 1930, 2036, 2140, 2180, 2301, 2498, 2656, 2715, 2951, 3348, 3490, 4076, 4189, 4943, 5682, 6573, 7874, 9806, 11853, 15115, 19459, 25523, 33764, 44958, 59800, 79143, 104298};
@@ -190,7 +190,7 @@ auto dsp_cut = dspdf.Filter(cut_ipchi2, {"Dsplus_IPCHI2_OWNPV"})
 		diffhist->SetBinContent(i+1, diff);
 
 	}
-	auto can = new TCanvas("cab", "can", 1600, 1200);
+	auto can = new TCanvas("can", "can", 1600, 1200);
 		can->cd();
 		//can->SetLeftMargin(0.15);
 		//can->SetRightMargin(0.09);
@@ -201,6 +201,60 @@ auto dsp_cut = dspdf.Filter(cut_ipchi2, {"Dsplus_IPCHI2_OWNPV"})
 		pad1->SetLogy();
 	diffhist->Draw();
 	can->SaveAs("image/aaa_overlap_difference.png");
+*/
+
+
+
+//looking at entries per run number to see why Ds seems to have more data than D+
+/*
+auto dpMin = dp_cut.Min("runNumber");
+auto dpMax = dp_cut.Max("runNumber");
+int dpMinRunN = *dpMin;
+int dpMaxRunN = *dpMax;
+cout << "Dplus min run number: " << dpMinRunN << "\nDplus max run number: " << dpMaxRunN << endl;
+auto dspMin = dsp_cut.Min("runNumber");
+auto dspMax = dsp_cut.Max("runNumber");
+int dspMinRunN = *dspMin;
+int dspMaxRunN = *dspMax;
+cout << "Ds min run number: " << dspMinRunN << "\nDs max run number: " << dspMaxRunN << endl;
+*/     //both cut and uncut, d+ and ds, had a range of (19228, 202897)
+
+
+auto dpRunNumberHist = dpdf.Fill<unsigned int>(TH1D("dpRunNumberHist"," ", 11000, 192000, 203000), {"runNumber"});
+auto dspRunNumberHist = dspdf.Fill<unsigned int>(TH1D("dspRunNumberHist"," ", 11000, 192000, 203000), {"runNumber"});
+
+auto dpRunNumberHistcut = dp_cut.Fill<unsigned int>(TH1D("dpRunNumberHistcut"," ", 11000, 192000, 203000), {"runNumber"});
+auto dspRunNumberHistcut = dsp_cut.Fill<unsigned int>(TH1D("dspRunNumberHistcut"," ", 11000, 192000, 203000), {"runNumber"});
+
+std::cout << "runN\tuc(D+)\tuc(Ds)\tc(D+)\tc(Ds)" << endl;
+int runN[11000];
+int dpCount[11000];
+int dspCount[11000];
+int dpCountcut[11000];
+int dspCountcut[11000];
+
+for (int i=1; i<11000; i++) {
+	int runNi = dspRunNumberHist->GetBinCenter(i);
+	int dpCounti = dpRunNumberHist->GetBinContent(i);
+	int dspCounti = dspRunNumberHist->GetBinContent(i);
+	int dpCountcuti = dpRunNumberHistcut->GetBinContent(i);
+	int dspCountcuti = dspRunNumberHistcut->GetBinContent(i);
+	runN[i] = runNi;
+	dpCount[i] = dpCounti;
+	dspCount[i] = dspCounti;
+	dpCountcut[i] = dpCountcuti;
+	dspCountcut[i] = dspCountcuti;
+
+	if (dpCounti > 0 || dspCounti > 0) {
+	std::cout << std::dec << std::fixed << std::cout.precision(0);
+	std::cout << runNi << "\t" << dpCounti << "\t" << dspCounti << "\t" << dpCountcuti << "\t" << dspCountcuti << endl;
+	}
+}
+
+
+
+
+
 
 
 
