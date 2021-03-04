2b44aef5cbcd3def9b040b1842659c9e9654ce83
John Bodenschatz(Mike student) <bodensjc@mail.uc.edu>
2021-03-04 15:16:54 -0500

    workign on creating the final cut ntuple for fitting

---
+++ dp_dsp_cut_rdf.C
@@ -0,0 +1,110 @@
+{//begin
+#include <TMath.h>
+#include <math.h>
+#include "scripts/fit1MeV_GaussianPlusCBWithExp_redo.C"
+
+using namespace std;
+using namespace ROOT;
+
+EnableImplicitMT();
+
+
+//create rdataframe for dp, dsp magdown data
+
+RDataFrame dpdf("D2KKpi/DecayTree", {"/share/lazy/D2KKpi/dpmagup_dec20.root","/share/lazy/D2KKpi/dpmagdown_dec20.root"});
+RDataFrame dspdf("Dsp2KKpi/DecayTree", {"/share/lazy/D2KKpi/dspmagup_dec20.root","/share/lazy/D2KKpi/dspmagdown_dec20.root"});
+
+
+//smaller test sets
+/*
+RDataFrame dpdf("D2KKpi/DecayTree", "/share/lazy/D2KKpi/dp_data_dec20/dpmagup01.root");
+RDataFrame dspdf("Dsp2KKpi/DecayTree", "/share/lazy/D2KKpi/dsp_data_dec20/dspmagdown01.root");
+*/
+
+
+
+const double phi_pm = 10;//12 for regular cut
+const double phiupperbound = (1019.455+phi_pm)*(1019.455+phi_pm)/1000000; 
+const double philowerbound = (1019.455-phi_pm)*(1019.455-phi_pm)/1000000; 
+const int cc = 299792458;//speed of light in m/s
+
+const int binmin = 1790;//1790 absolute minimum for dp
+const int binmax = 2050;//max value for dsp is 2050
+const int nbins = binmax-binmin;//to get events / MeV, should be 260 bins
+
+cout << "there are " << nbins << " bins" << endl;
+
+
+
+
+
+auto inv_m_func = [](double px1, double py1, double pz1, double pe1, double px2, double py2, double pz2, double pe2) {return TLorentzVector(TLorentzVector(px1, py1, pz1, pe1)+TLorentzVector(px2, py2, pz2, pe2)).Mag2()/1000000 ;};
+auto prob_func = [](double prob1, double prob2) {return TMath::Log(prob1) - TMath::Log(prob2) ;};
+auto probNNx_func = [](double prob1, double prob2, double prob3) {return prob1*prob2*prob3 ;};
+
+
+auto cut_ipchi2 = [](double x) {return x < 4 ;};//5 for regular cut
+auto cut_fdchi2 = [](double x) {return x > 225 ;};//175 for regular cut
+auto cut_endvertexchi2 = [](double x) {return x < 9 ;};
+auto cut_phi = [&phiupperbound, &philowerbound](double x) {return x > philowerbound && x < phiupperbound ;};
+auto cut_prob_5 = [] (double x) {return x>5 ;};
+auto cut_prob_0 = [] (double x) {return x>0 ;};
+auto cut_probnnx = [] (double x) {return x >= 0.70 ;};
+auto cut_pK_ambiguity = [] (double x) {return x> 10000 ;}; //10,000 MeV = 10GeV
+auto cut_dira = [] (double x) {return x>0.99999 ;};
+
+
+
+
+auto dp_cut = dpdf.Filter(cut_ipchi2, {"Dplus_IPCHI2_OWNPV"})
+				   .Filter(cut_fdchi2, {"Dplus_FDCHI2_OWNPV"})
+				   .Filter(cut_endvertexchi2, {"Dplus_ENDVERTEX_CHI2"})
+				   .Filter(cut_pK_ambiguity, {"Kplus_P"})
+				   .Filter(cut_pK_ambiguity, {"Kminus_P"})
+				   .Filter(cut_dira, {"Dplus_DIRA_OWNPV"})
+				   .Define("kminuslog", prob_func, {"Kminus_MC15TuneV1_ProbNNk", "Kminus_MC15TuneV1_ProbNNpi"})
+				   .Define("kpluslog", prob_func, {"Kplus_MC15TuneV1_ProbNNk", "Kplus_MC15TuneV1_ProbNNpi"})
+				   .Define("pipluslog", prob_func, {"Piplus_MC15TuneV1_ProbNNpi", "Piplus_MC15TuneV1_ProbNNk"})
+				   .Filter(cut_prob_5, {"kminuslog"})
+				   .Filter(cut_prob_5, {"kpluslog"})
+				   .Filter(cut_prob_0, {"pipluslog"})
+				   .Define("ProbNNx", probNNx_func, {"Kminus_MC15TuneV1_ProbNNk", "Kplus_MC15TuneV1_ProbNNk", "Piplus_MC15TuneV1_ProbNNpi"})
+				   .Filter(cut_probnnx, {"ProbNNx"})
+				   .Define("mkpkm", inv_m_func, {"Kplus_PX", "Kplus_PY", "Kplus_PZ", "Kplus_PE", "Kminus_PX", "Kminus_PY", "Kminus_PZ", "Kminus_PE"})
+				   .Filter(cut_phi, {"mkpkm"});
+
+
+auto dsp_cut = dspdf.Filter(cut_ipchi2, {"Dsplus_IPCHI2_OWNPV"})
+				   .Filter(cut_fdchi2, {"Dsplus_FDCHI2_OWNPV"})
+				   .Filter(cut_endvertexchi2, {"Dsplus_ENDVERTEX_CHI2"})
+				   .Filter(cut_pK_ambiguity, {"Kplus_P"})
+				   .Filter(cut_pK_ambiguity, {"Kminus_P"})
+				   .Filter(cut_dira, {"Dsplus_DIRA_OWNPV"})
+				   .Define("kminuslog", prob_func, {"Kminus_MC15TuneV1_ProbNNk", "Kminus_MC15TuneV1_ProbNNpi"})
+				   .Define("kpluslog", prob_func, {"Kplus_MC15TuneV1_ProbNNk", "Kplus_MC15TuneV1_ProbNNpi"})
+				   .Define("pipluslog", prob_func, {"Piplus_MC15TuneV1_ProbNNpi", "Piplus_MC15TuneV1_ProbNNk"})
+				   .Filter(cut_prob_5, {"kminuslog"})
+				   .Filter(cut_prob_5, {"kpluslog"})
+				   .Filter(cut_prob_0, {"pipluslog"})
+				   .Define("ProbNNx", probNNx_func, {"Kminus_MC15TuneV1_ProbNNk", "Kplus_MC15TuneV1_ProbNNk", "Piplus_MC15TuneV1_ProbNNpi"})
+				   .Filter(cut_probnnx, {"ProbNNx"})
+				   .Define("mkpkm", inv_m_func, {"Kplus_PX", "Kplus_PY", "Kplus_PZ", "Kplus_PE", "Kminus_PX", "Kminus_PY", "Kminus_PZ", "Kminus_PE"})
+				   .Filter(cut_phi, {"mkpkm"});
+
+
+
+//need to make ntuples for the cut data
+//one with D+ in overlap, one with Ds in overlap
+//per image: overlap_difference, use 1896 - 1944 as edges for overlap
+
+
+
+
+
+
+
+
+}//end
+
+
+
